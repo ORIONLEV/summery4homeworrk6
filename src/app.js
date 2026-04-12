3a193f6c13bc81a0a05c1aedb3016723358d5045
Lev <lev2010alex@gmail.com>
2026-04-12 20:28:57 +0200

    first

---
+++ src/app.js
@@ -0,0 +1,16 @@
+// import plus from "./js/calculator"
+// console.log(plus(5 , 10 ));
+
+
+// import { minus } from "./js/calculator";
+// console.log(minus(15,5));
+
+
+// import calc from "./js/calculator"
+// console.log(calc.minus(10,5));
+// console.log(calc.add(10,5));
+
+import { add, minus } from "./js/calculator";
+console.log(add(30,25));
+console.log(minus(30,25));
+
---
+++ src/js/calculator.js
@@ -0,0 +1,25 @@
+// export default function add(a,b){  // берет только одну вешь
+//     return a+b; 
+// }
+
+// export function minus(a,b){
+//     return a-b; 
+// }
+
+
+// function minus(a,b){
+//     return a-b; 
+// }
+// function add(a,b){
+//     return a+b; 
+// }
+// export default {minus ,add}
+
+
+export function minus(a,b){
+    return a-b; 
+}
+export function add(a,b){
+    return a+b; 
+}
+
