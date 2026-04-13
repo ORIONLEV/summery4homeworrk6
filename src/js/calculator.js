ab50355d83bc58eaf3420d5b3b2cc109fdaadc07
Lev <lev2010alex@gmail.com>
2026-04-13 22:05:30 +0200

    idk

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,21 +1,3 @@
-// export default function add(a,b){  // берет только одну вешь
-//     return a+b; 
-// }
-
-// export function minus(a,b){
-//     return a-b; 
-// }
-
-
-// function minus(a,b){
-//     return a-b; 
-// }
-// function add(a,b){
-//     return a+b; 
-// }
-// export default {minus ,add}
-
-
 export function minus(a,b){
     return a-b; 
 }
